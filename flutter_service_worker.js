80f250672125559fb0a39416beac5cd50bbc79e1
gpfla8966 <gpfla8966@gmail.com>
2026-03-22 05:53:22 +0900

    Deploy: improved news analysis (sector keywords)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "e9b225d6b76424add1bd70f81775d505",
+"flutter_bootstrap.js": "72574439cde6b28124e1cde5e1581c5c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "253e5de38a77086913f1804f49320876",
 "/": "253e5de38a77086913f1804f49320876",
-"main.dart.js": "084468bba117f03354d3e2a2349d7fc6",
+"main.dart.js": "684532d1d0e191a4f2ee05624c2f5a37",
 "manifest.json": "9e89c404fbdc3365aa1013da352a6fa6",
 "version.json": "dadb4e551b30e0000271d036a2301860"};
 // The application shell files that are downloaded before a service worker can
