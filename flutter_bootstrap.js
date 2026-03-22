2dd48291ff789bd2cf8d178deaa91d7953e22ba1
gpfla8966 <gpfla8966@gmail.com>
2026-03-22 10:38:54 +0900

    v1.2.1: 매도 권장 시점 (단기/장기) 날짜 추가

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1174888333"
+    serviceWorkerVersion: "3921226015"
   }
 });
