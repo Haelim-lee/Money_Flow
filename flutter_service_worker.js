7659b41ef74e2014e8d96f164aac35551dc96d12
gpfla8966 <gpfla8966@gmail.com>
2026-03-22 10:31:42 +0900

    v1.2.1: 기술적분석 시간대 예측 추가 (상승/하락 예상 시간대, 고점 예상)

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "2cb836d84c76cef694985c516ffbd46a",
+"flutter_bootstrap.js": "064e6d9802d53446ac53996e65526849",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "253e5de38a77086913f1804f49320876",
 "/": "253e5de38a77086913f1804f49320876",
-"main.dart.js": "8fa923314ba795ca1802fb981768da3d",
+"main.dart.js": "bfdd520f690007aa962994146dbc9adf",
 "manifest.json": "9e89c404fbdc3365aa1013da352a6fa6",
 "version.json": "dadb4e551b30e0000271d036a2301860"};
 // The application shell files that are downloaded before a service worker can
